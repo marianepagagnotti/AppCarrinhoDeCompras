1bd88c82c8f566330df666b3fe906a2d241aa18b
Usuario <marianepagagnotti.sl@gmail.com>
2022-11-09 15:34:28 -0300

    Adicionar arquivos de projeto.

---
+++ AppCarrinhoDeCompras/AppCarrinhoDeCompras/Helper/SQLiteDatabaseHelper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using AppCarrinhoDeCompras.Model;
+using SQLite;
+
+namespace AppCarrinhoDeCompras.Helper
+{
+    public class SQLiteDatabaseHelper
+    {
+
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDatabaseHelper(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path);
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+        public Task<int> insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public void update(Produto p)
+        {
+            string sql = "UPDATE Produto SET descricao= ?, quantidade= ?, preco= ? WHERE id= ?";
+            _conn.QueryAsync<Produto>(sql, p.descricao, p.quantidade, p.preco, p.id);
+        }
+
+        /*public Task<Produto> getById(int p)
+        {
+            return new Produto();
+        }*/
+
+        public Task<List<Produto>> getAll()
+        {
+            return _conn.Table<Produto>().ToListAsync();
+        }
+
+        public Task<int> delete(int id)
+        {
+            return _conn.Table<Produto>().DeleteAsync(i => i.id == id);
+        }
+    }
+}
---
+++ AppCarrinhoDeCompras/AppCarrinhoDeCompras/View/Listagem.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AppCarrinhoDeCompras.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Listagem : ContentPage
+    {
+        public Listagem()
+        {
+            InitializeComponent();
+        }
+
+        private void ToolbarItem_Clicked_Novo(object sender, EventArgs e)
+        {
+            try
+            {
+                Navigation.PushAsync(new NovoProduto());
+            }catch(Exception ex)
+            {
+                DisplayAlert("Ops", ex.Message, "OK");
+            }
+        }
+
+        private void ToolbarItem_Clicked_Somar(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
